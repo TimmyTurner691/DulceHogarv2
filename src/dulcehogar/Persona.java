a50277f306853c82dea647b077f3278f1529547a
turne142 <jake.turner@mail.utoronto.ca>
2024-11-25 21:49:07 -0300

    Super class Persona creado.

---
+++ src/dulcehogar/Persona.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dulcehogar;
+
+/**
+ *
+ * @author jaket
+ */
+public class Persona {
+    private final String rut;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String domicilio;
+    private String comuna;
+    private String telefono;
+
+    public Persona(String rut) {
+        this.rut = rut;
+        this.nombre = "";
+        this.apellidoPaterno = "";
+        this.apellidoMaterno = "";
+        this.domicilio = "";
+        this.comuna = "";
+        this.telefono = "";
+    }
+
+    // Getters and setters for optional fields
+    public String getRut() {
+        return rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
--- src/dulcehogar/Socio.java
+++ src/dulcehogar/Socio.java
@@ -8,28 +8,15 @@ package dulcehogar;
  *
  * @author jaket
  */
-public class Socio {
+public class Socio extends Persona {
     private final int numeroSocio;
-    private final String rut;
-    private String nombre;
-    private String apellidoPaterno;
-    private String apellidoMaterno;
-    private String domicilio;
-    private String comuna;
-    private String telefono;
     private int valorCuota;
     private int cantidadAportada;
     private int numeroCuota;
 
     public Socio(int numeroSocio, String rut) {
+        super(rut); 
         this.numeroSocio = numeroSocio;
-        this.rut = rut;
-        this.nombre = "";
-        this.apellidoPaterno = "";
-        this.apellidoMaterno = "";
-        this.domicilio = "";
-        this.comuna = "";
-        this.telefono = "";
         this.valorCuota = 0;
         this.cantidadAportada = 0;
         this.numeroCuota = 0;
@@ -44,58 +31,6 @@ public class Socio {
     
     public int getNumeroSocio() {
         return numeroSocio;
-    }
-
-    public String getRut() {
-        return rut;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidoPaterno() {
-        return apellidoPaterno;
-    }
-
-    public void setApellidoPaterno(String apellidoPaterno) {
-        this.apellidoPaterno = apellidoPaterno;
-    }
-
-    public String getApellidoMaterno() {
-        return apellidoMaterno;
-    }
-
-    public void setApellidoMaterno(String apellidoMaterno) {
-        this.apellidoMaterno = apellidoMaterno;
-    }
-
-    public String getDomicilio() {
-        return domicilio;
-    }
-
-    public void setDomicilio(String domicilio) {
-        this.domicilio = domicilio;
-    }
-
-    public String getComuna() {
-        return comuna;
-    }
-
-    public void setComuna(String comuna) {
-        this.comuna = comuna;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
     }
 
     public int getValorCuota() {
@@ -116,33 +51,5 @@ public class Socio {
 
     public void setNumeroCuota(int numeroCuota) {
         this.numeroCuota = numeroCuota;
-    }
-    
-    
-    public  String showSocio() {
-        return "Número de Socio: " + numeroSocio + "\n" +
-               "RUT: " + rut + "\n" +
-               "Nombre: " + nombre + "\n" +
-               "Apellido Paterno: " + apellidoPaterno + "\n" +
-               "Apellido Materno: " + apellidoMaterno + "\n" +
-               "Domicilio: " + domicilio + "\n" +
-               "Comuna: " + comuna + "\n" +
-               "Teléfono: " + telefono + "\n" +
-               "Valor Cuota: $" + valorCuota + "\n" +
-               "Cantidad Aportada: $" + cantidadAportada;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Socio{" + "numeroSocio=" + numeroSocio + ", rut=" + rut + 
-                ", nombre=" + nombre + ", apellidoPaterno=" + apellidoPaterno + 
-                ", apellidoMaterno=" + apellidoMaterno + 
-                ", domicilio=" + domicilio + ", comuna=" + comuna + ", telefono=" + telefono + 
-                ", valorCuota=" + valorCuota + ", cantidadAportada=" + 
-                cantidadAportada + '}';
-    }
-
-    
-    
+    }   
 }
